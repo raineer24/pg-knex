7ac89424416d8ff198d4120a44cfd098f688b449
raineer <delaritaraineer81@gmail.com>
2019-12-04 15:32:17 +0800

    verify token heroku

--- api/routes/users.js
+++ api/routes/users.js
@@ -15,6 +15,32 @@ const User = require("../../models/users");
 
 const Mailer = require("../../service/mail");
 
+const multer = require("multer");
+
+const cloudinary = require("cloudinary");
+
+const storage = multer.diskStorage({
+  filename(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+
+const imageFilter = (req, file, cb) => {
+  // accept image files only
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
+
+const upload = multer({ storage, fileFilter: imageFilter });
+
+cloudinary.config({
+  cloud_name: "dwsbpkgvr",
+  api_key: "246382268158277",
+  api_secret: "OEJwFk8xMOuNID7Z7L5MNDJ9nY8"
+});
+
 require("dotenv").config();
 
 let that;
@@ -26,7 +52,7 @@ router.get("/", (req, res, next) => {
     .then(data => res.json(data));
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", upload.single("image"), (req, res) => {
   that = this;
 
   // Ensures that all entries by the user are valid
@@ -36,86 +62,93 @@ router.post("/register", (req, res) => {
   if (!isValid) {
     return res.status(400).json(errors);
   }
-
-  let token;
-  crypto.randomBytes(48, (err, buf) => {
-    if (err) throw err;
-    token = buf
-      .toString("base64")
-      .replace(/\//g, "") // Because '/' and '+' aren't valid in URLs
-      .replace(/\+/g, "-");
-    return token;
-  });
-
-  bcrypt.genSalt(12, (err, salt) => {
-    if (err) throw err;
-    bcrypt.hash(req.body.password, salt, (err, hash) => {
+  cloudinary.uploader.upload(req.file.path, result => {
+    let token;
+    crypto.randomBytes(48, (err, buf) => {
       if (err) throw err;
-      database("users")
-        .returning(["id", "email", "username", "token", "first_name"])
-        .insert({
-          email: req.body.email,
-          password: hash,
-          username: req.body.username,
-          token: token,
-          first_name: req.body.first_name,
-          image_url: req.body.image_url,
-          emailverified: "f",
-          tokenusedbefore: "f"
-        })
-        .then(user => {
-          console.log("user[0]", user[0]);
-          console.log(user.token);
+      token = buf
+        .toString("base64")
+        .replace(/\//g, "") // Because '/' and '+' aren't valid in URLs
+        .replace(/\+/g, "-");
+      return token;
+    });
 
-          const hostname = JSON.stringify(config.env.port).replace(/"/g, "");
-          // const cfg =
-          //   `http://localhost:${hostname}/api/v2/users/verify/` + user[0].token;
+    bcrypt.genSalt(12, (err, salt) => {
+      if (err) throw err;
+      bcrypt.hash(req.body.password, salt, (err, hash) => {
+        if (err) throw err;
+        database("users")
+          .returning(["id", "email", "username", "token", "first_name"])
+          .insert({
+            email: req.body.email,
+            password: hash,
+            username: req.body.username,
+            token: token,
+            first_name: req.body.first_name,
+            image_url: result.secure_url,
+            emailverified: "f",
+            tokenusedbefore: "f"
+          })
+          .then(user => {
+            console.log("user[0]", user[0]);
+            console.log(user.token);
 
-          //const cfg = `http://localhost:4200/#/users/verify/` + user[0].token;
+            // const hostname = JSON.stringify(config.env.port).replace(/"/g, "");
 
-          //const cfg = `http://localhost:4200/#/users/verify/`;
+            const hostname =
+              config.env.hostname === "localhost"
+                ? `${config.env.hostname}:${config.env.port}`
+                : config.env.hostname;
 
-          const cfg = `http://localhost:4200/#/users/verify/?token=${user[0].token}`;
+            // const cfg =
+            //   `http://localhost:${hostname}/api/v2/users/verify/` + user[0].token;
 
-          console.log(cfg);
+            //const cfg = `http://localhost:4200/#/users/verify/` + user[0].token;
 
-          let username = JSON.stringify(user[0].first_name);
-          console.log(username);
+            //const cfg = `http://localhost:4200/#/users/verify/`;
 
-          let newTemp = username.replace(/"/g, "");
+            const cfg = `http://${hostname}/#/users/verify/?token=${user[0].token}`;
 
-          const body = `<table><div><p>Hi, ${newTemp}</p></div>
+            console.log(cfg);
+
+            let username = JSON.stringify(user[0].first_name);
+            console.log(username);
+
+            let newTemp = username.replace(/"/g, "");
+
+            const body = `<table><div><p>Hi, ${newTemp}</p></div>
           <div><p><a href="${cfg}">Registration Link</p></div></table>`;
 
-          const mailOptions = {
-            from: '"Example Team" <delaritaraineer81@gmail.com>',
-            to: "delaritaraineer81@gmail.com, delaritaraineer81@gmail.com",
-            subject: "Rain - Successful registration Encrypt  v2",
-            text: "Hey there, it’s our first message sent with Nodemailer ",
-            html: body
-          };
+            const mailOptions = {
+              from: '"Example Team" <delaritaraineer81@gmail.com>',
+              to: "delaritaraineer81@gmail.com, delaritaraineer81@gmail.com",
+              subject: "Rain - Successful registration Encrypt  v2",
+              text: "Hey there, it’s our first message sent with Nodemailer ",
+              html: body
+            };
 
-          new Mailer(mailOptions)
-            .send()
-            .then(() => {})
-            .catch(err => {
-              console.log(err);
+            new Mailer(mailOptions)
+              .send()
+              .then(() => {})
+              .catch(err => {
+                console.log(err);
 
-              console.log("failed to send");
-            });
+                console.log("failed to send");
+              });
 
-          // This is where the api returns json to the /register route
-          // Return the id, email, registered on date and token here
-          // Sending the user's token as a response here is insecure
-          res.json({ user, success: true, message: "ok" });
-          // console.log(user[0]);
-        })
-        .catch(err => {
-          console.log(err);
+            // This is where the api returns json to the /register route
+            // Return the id, email, registered on date and token here
+            // Sending the user's token as a response here is insecure
+            res.json({ user, success: true, message: "ok" });
+            // console.log(user[0]);
+          })
+          .catch(err => {
+            console.log(err);
 
-          errors.account = "Email already registered";
-          res.status(400).json(errors);
-        });
+            errors.account = "Email already registered";
+            res.status(400).json(errors);
+          });
+      });
     });
   });
 });
