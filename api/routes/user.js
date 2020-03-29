0f4afb87248d3d895ef1e8403e87f8cddb442468
raineer <delaritaraineer81@gmail.com>
2020-03-29 22:54:14 +0800

    post route register

--- api/routes/user.js
+++ api/routes/user.js
@@ -8,4 +8,36 @@ router.get("/", (req, res) => {
   });
 });
 
+router.post("/register", (req, res) => {
+  console.log(req.body);
+
+  database("users")
+    .returning(["id", "first_name", "email", "image_url", "username"])
+    .insert({
+      email: req.body.email,
+      password: req.body.password,
+      username: req.body.username,
+      token: "token",
+      first_name: req.body.first_name,
+      image_url: req.body.email,
+      emailverified: "f",
+      tokenusedbefore: "f"
+    })
+    .then(user => {
+      // This is where the api returns json to the /register route
+      // Return the id, email, registered on date and token here
+      // Sending the user's token as a response here is insecure
+      return res.status(201).json({ user, success: true, message: "Saved" });
+      // console.log(user[0]);
+    })
+    .catch(err => {
+      if (err.routine === "_bt_check_unique") {
+        return res
+          .status(400)
+          .send({ message: "User with that EMAIL already exist" });
+      }
+      return res.status(400).send(err);
+    });
+});
+
 module.exports = router;
