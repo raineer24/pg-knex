dbd60d96db9523a2cb4fad5141c651ab972b94de
raineer <delaritaraineer81@gmail.com>
2020-04-04 00:26:26 +0800

    user now found

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -5,31 +5,50 @@ const log = require("color-logs")(true, true, "User Account");
 
 //const postLogin = ()
 
-const postLogin = async (req, res, next) => {
+const postLogin = (req, res, next) => {
   const email = String(req.body.email);
   const password = String(req.body.password);
-  try {
-    let user = await _findUserByEmail({ email });
-    const isMatch = await bcrypt.compare(password, user.password);
-  } catch (err) {
-    log.error(err.message);
-    res.status(500).send(`Server error: ${err.message}`);
-  }
-  if (!isMatch) {
-    return res.status(400).json({ errors: [{ msg: "Password incorrect" }] });
-  }
+  console.log(req.body);
 
-  const payload = { id: user.id, email: user.email };
-  jwt.sign(
-    payload,
-    process.env.SECRET_KEY,
-    { expiresIn: "1h" },
-    (err, token) => {
-      if (err) throw err;
-      log.info(`Logged into user account ${user.email}`);
-      res.status(200).json({ message: " Auth Successful", token: token });
-    }
-  );
+  User.query()
+    .where("email", email)
+    .first()
+    .then(user => {
+      console.log(`user!: `, user);
+
+      if (!user) {
+        return Promise.reject(new Error("User not found"));
+      }
+      return Promise.resolve(user);
+    });
+
+  // let user = await _findUserByEmail({ email });
+  //console.log(user);
+
+  // try {
+  //   let user = await _findUserByEmail({ email });
+  //   console.log(user);
+
+  //   const isMatch = await bcrypt.compare(password, user.password);
+
+  //   const payload = { id: user.id, email: user.email };
+  //   jwt.sign(
+  //     payload,
+  //     process.env.SECRET_KEY,
+  //     { expiresIn: "1h" },
+  //     (err, token) => {
+  //       if (err) throw err;
+  //       log.info(`Logged into user account ${user.email}`);
+  //       res.status(200).json({ message: " Auth Successful", token: token });
+  //     }
+  //   );
+  // } catch (err) {
+  //   log.error(err.message);
+  //   res.status(500).send(`Server error: ${err.message}`);
+  // }
+  // if (!isMatch) {
+  //   return res.status(400).json({ errors: [{ msg: "Password incorrect" }] });
+  // }
 };
 
 const _findUserByEmail = email => {
@@ -48,7 +67,7 @@ const _findUserByEmail = email => {
 
 const getCurrent = (req, res, next) => {
   res
-    .json({
+    .send({
       message: "Welcome Test Development"
     })
     .catch(next);
