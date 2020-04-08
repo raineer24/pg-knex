9575840f94b6e4a6900d9b883d11ec937fff7138
raineer <delaritaraineer81@gmail.com>
2020-04-08 22:17:05 +0800

    error codes

--- api/controllers/auth_controller.js
+++ api/controllers/auth_controller.js
@@ -9,28 +9,31 @@ const {
   GENERIC_ERROR
 } = require("../../helpers/error_helper");
 const asyncWrapper = require("../../middleware/asyncWrapper");
+const handler = require("../../utils/responseHandler");
+
 /**
  * Signin
  * @param {object} req
  * @param {object} res
  * @returns {object} user object
  */
-const postLogin = asyncWrapper(async (req, res, next) => {
+const postLogin = async (req, res, next) => {
   const email = String(req.body.email);
   const password = String(req.body.password);
   console.log(req.body);
 
   try {
+    let rows = await User.query().where("email", email);
+    console.log("rows", rows);
+
+    const user = rows[0];
+
+    if (!user) return handler.errorMessage(res, "lol, wtf");
+
+    console.log(user);
+
     if (!email || !password) {
       let err = new Error("`username` + `password` are required fields");
-      err.context = req.body.email;
-      err.status = BAD_REQUEST;
-      throw err;
-    }
-
-    let user = await _findUserByEmail(email);
-    if (!user) {
-      let err = new Error("`email doesn't exist");
       err.context = req.body.email;
       err.status = BAD_REQUEST;
       throw err;
@@ -40,7 +43,7 @@ const postLogin = asyncWrapper(async (req, res, next) => {
 
     return next(err);
   }
-});
+};
 
 // throwError = (code, errorType, errorMessage) => error => {
 //   if (!error) error = new Error(errorMessage || "Default Error");
@@ -49,19 +52,16 @@ const postLogin = asyncWrapper(async (req, res, next) => {
 //   throw error;
 // };
 
-const _findUserByEmail = email =>
-  User.query()
-    .where("email", email)
-    .then(data => {
-      let user = data[0];
-      if (!user) {
-        let err = new Error("User not found!");
-        // err.context = req.body.email;
-        err.status = BAD_REQUEST;
-        throw err;
-      }
-      return user;
-    });
+// const signInUserQuery = await User.query()
+//   .where("email", email)
+//   .then(data => {
+//     let user = data[0];
+//     if (!user) {
+//       //return Promise.reject(new Error("User not found"));
+//       return handler.errorMessage(res, "lol, wtf");
+//     }
+//     return Promise.resolve(user);
+//   });
 
 // .then(user => {
 //   console.log("iser", user);
--- helpers/status..js
+++ helpers/status..js
@@ -1,7 +1,13 @@
-const succesMessage = { status: 'success'};
-const errorMessage = { status: 'error'};
+const successMessage = { status: "success" };
+const errorMessage = { status: "error" };
 const status = {
   succes: 200,
   error: 500,
-  notfound: 404
-}
+  notfound: 404,
+  unauthorized: 401,
+  created: 201,
+  bad: 400,
+  nocontent: 204
+};
+
+export { successMessage, errorMessage, status };
