63ccd5f0855ac38fae1ed87186718929fcb19dd6
raineer <delaritaraineer81@gmail.com>
2020-04-23 22:29:53 +0800

    password equals to password 2

--- validation/register.js
+++ validation/register.js
@@ -4,8 +4,28 @@ const isEmpty = require("./is-empty");
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
-  if (!Validator.isLength(data.username, { min: 2, max: 30 })) {
-    errors.name = "Username must be between 2 and 30 characters";
+  data.username = !isEmpty(data.username) ? data.username : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+
+  if (!Validator.isLength(data.username)) {
+    errors.name = "Username field is required";
+  }
+  if (!Validator.isLength(data.email)) {
+    errors.email = "Email is invalid";
+  }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Password field is required";
+  }
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm Password field is required";
+  }
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Password must match";
   }
 
   return {
