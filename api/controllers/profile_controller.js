a231634368703ddc9a56f0a436da7cb39aff6ff0
raineer <delaritaraineer81@gmail.com>
2020-05-12 19:07:48 +0800

    no profile undefined

--- api/controllers/profile_controller.js
+++ api/controllers/profile_controller.js
@@ -2,15 +2,36 @@ const UserProfile = require("../../models/user_profile");
 const User = require("../../models/users");
 const passport = require("passport");
 
+const {
+  createError,
+  BAD_REQUEST,
+  UNAUTHORIZED,
+  CONFLICT
+} = require("../../helpers/error_helper");
+
 const getTest = (req, res, next) => {
   res.json({ msg: "Profile works" });
 };
 
-const getProfile = (req, res, next) => {
-  UserProfile.query()
-    .findById(req.user.id)
-    .eager("user")
-    .then(userprofile => res.json(userprofile));
+const getProfile = async (req, res, next) => {
+  try {
+    const user = UserProfile.query()
+      .findById(req.user.id)
+      .eager("user")
+      .then(userprofile => {
+        console.log("userprofile", userprofile);
+        if (userprofile === undefined) {
+          return next(
+            createError({
+              status: CONFLICT,
+              message: "There is no profile for this user"
+            })
+          );
+        }
+      });
+  } catch (error) {
+    throw error;
+  }
 };
 
 const createProfile = (req, res, next) => {
