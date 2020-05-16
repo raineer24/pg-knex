1de56ed3a2e92d87309edd3ab19fe3ef4d05ac13
raineer <delaritaraineer81@gmail.com>
2020-05-16 19:01:37 +0800

    express validotr for user profile

--- validation/express-register.js
+++ validation/express-register.js
@@ -1,9 +1,5 @@
-//const expressValidator = require("express-validator");
 const { check } = require("express-validator");
 const { validationResult } = require("express-validator");
-// module.exports = data = (req, res, next) => {
-//   console.log("data.username: ", data);
-// };
 
 exports.validateUser = [
   check("email")
@@ -53,28 +49,3 @@ exports.validateUser = [
     next();
   }
 ];
-
-// module.exports = function(data) {
-//   console.log("data.username: ", data);
-//   req.checkbody("email", "Email is required").notEmpty();
-//   const email = data.email;
-//   //console.log("email"), email;
-
-//   return async (req, res, next) => {
-//     //console.log("req.body.email", await req.body.email);
-//     // req.checkbody("email", "Email is required").notEmpty();
-//     // req.getValidationResult();
-//   };
-// };
-
-// module.exports = async function(req, res, next) {
-//   return new Promise((resolve, reject) => {
-//     const foo = req.body.username;
-//     resolve(foo);
-//   });
-// };
-
-// module.exports = data =>
-//   async function(req, res, next) {
-//     console.log("data.username: ", req.body);
-//   };
