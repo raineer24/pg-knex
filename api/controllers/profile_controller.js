0fb57a7f30392df3e9885e3ca4b7578eec4d6cdb
raineer <delaritaraineer81@gmail.com>
2020-05-16 15:52:15 +0800

    update patch profileuser

--- api/controllers/profile_controller.js
+++ api/controllers/profile_controller.js
@@ -101,25 +101,13 @@ const createProfile = async (req, res, next) => {
   // console.log("data", user);
 
   async function saveProfile() {
-    // const newSale = {
-    //   subtotal: 10,
-    //   taxes: 8,
-    //   total: 18,
-    //   // property details because that's how we
-    //   // call it in the relationMappings
-    //   details: [
-    //     {
-    //       product: "Tomato",
-    //       quantity: 1,
-    //       price: 4
-    //     },
-    //     {
-    //       product: "Potato",
-    //       quantity: 2,
-    //       price: 3
-    //     }
-    //   ]
-    // };
+    const updateJennifer = await jennifer
+      .$query()
+      .patch({ firstName: "J.", lastName: "Lawrence" })
+      .returning("*");
+
+    console.log(updateJennifer.updatedAt); // NOW()-ish
+    console.log(updateJennifer.firstName);
 
     // end here
     const profileUser = await UserProfile.query().insertGraph(data);
