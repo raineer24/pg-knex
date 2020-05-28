095e43a67728978868c1cb90404c5eb196e2cd32
raineer dela rita <rain@gmail.com>
2020-05-28 21:02:12 +0800

    unique constraint

--- db/migrations/20200528204359_user_exp_detail03.js
+++ db/migrations/20200528204359_user_exp_detail03.js
@@ -1,6 +1,6 @@
 exports.up = function(knex, Promise) {
     return knex.raw(`
-    CREATE TABLE IF NOT EXISTS user_profile 
+       CREATE TABLE IF NOT EXISTS user_profile 
              ( 
                           id SERIAL , 
                           users_id                  INTEGER, 
@@ -16,15 +16,38 @@ exports.up = function(knex, Promise) {
                           created_at TIMESTAMP with time zone NOT NULL DEFAULT now(), 
                           updated_at timestamp WITH time zone NOT NULL DEFAULT now(), 
                           PRIMARY KEY (id, users_id), 
-                          FOREIGN KEY (users_id) REFERENCES users (id) 
+                          FOREIGN KEY (users_id) REFERENCES users (id),
+				 		  UNIQUE(users_id)
+				 		  
              );
-                                   
+			 
+			
+CREATE TABLE IF NOT EXISTS user_experience_detail 
+  ( 
+     user_experience_detail_id SERIAL, 
+     usersp_id                 INTEGER REFERENCES user_profile(users_id), 
+     job_title                 VARCHAR(40) NOT NULL, 
+     company_name              VARCHAR(40) NOT NULL, 
+     job_location              VARCHAR(40), 
+     start_date                DATE NOT NULL, 
+     end_date                  DATE, 
+     current                   BOOLEAN DEFAULT false, 
+     description               TEXT, 
+     PRIMARY KEY (user_experience_detail_id, usersp_id) 
+  ); 
+
+CREATE TABLE IF NOT EXISTS user_skill_set 
+  ( 
+     user_skill_set_id SERIAL PRIMARY KEY, 
+     skill_set_name    JSON NOT NULL, 
+     FOREIGN KEY (user_skill_set_id) REFERENCES user_profile(users_id) 
+  ); 
       `);
   };
   
   exports.down = function(knex, Promise) {
     return knex.raw(`
-    CREATE TABLE IF NOT EXISTS user_profile 
+       CREATE TABLE IF NOT EXISTS user_profile 
              ( 
                           id SERIAL , 
                           users_id                  INTEGER, 
@@ -40,9 +63,32 @@ exports.up = function(knex, Promise) {
                           created_at TIMESTAMP with time zone NOT NULL DEFAULT now(), 
                           updated_at timestamp WITH time zone NOT NULL DEFAULT now(), 
                           PRIMARY KEY (id, users_id), 
-                          FOREIGN KEY (users_id) REFERENCES users (id) 
+                          FOREIGN KEY (users_id) REFERENCES users (id),
+				 		  UNIQUE(users_id)
+				 		  
              );
-                   
-        `);
+			 
+			
+CREATE TABLE IF NOT EXISTS user_experience_detail 
+  ( 
+     user_experience_detail_id SERIAL, 
+     usersp_id                 INTEGER REFERENCES user_profile(users_id), 
+     job_title                 VARCHAR(40) NOT NULL, 
+     company_name              VARCHAR(40) NOT NULL, 
+     job_location              VARCHAR(40), 
+     start_date                DATE NOT NULL, 
+     end_date                  DATE, 
+     current                   BOOLEAN DEFAULT false, 
+     description               TEXT, 
+     PRIMARY KEY (user_experience_detail_id, usersp_id) 
+  ); 
+
+CREATE TABLE IF NOT EXISTS user_skill_set 
+  ( 
+     user_skill_set_id SERIAL PRIMARY KEY, 
+     skill_set_name    JSON NOT NULL, 
+     FOREIGN KEY (user_skill_set_id) REFERENCES user_profile(users_id) 
+  ); 
+      `);
   };
   
