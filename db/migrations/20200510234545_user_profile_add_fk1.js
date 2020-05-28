c1dae3c0f927ade38fbbdb9fc07ed4d9b9b66e19
raineer dela rita <rain@gmail.com>
2020-05-28 15:10:25 +0800

    create migrate create userprofile

--- db/migrations/20200510234545_user_profile_add_fk1.js
+++ db/migrations/20200510234545_user_profile_add_fk1.js
@@ -1,29 +1,40 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("user_profile", table => {
-    table
-      .increments("id")
-      .references("id")
-      .inTable("users");
-    table.string("company_name");
-    table.string("website");
-    table.string("job_location");
-    table.string("status").notNullable();
-    table.string("bio");
-    table.string("youtube_handle");
-    table.string("twitter_handle");
-    table.string("facebook_handle");
-    table.string("instagram_handle");
-    table
-      .timestamp("created_at")
-      .notNullable()
-      .defaultTo(knex.fn.now());
-    table
-      .timestamp("updated_at")
-      .notNullable()
-      .defaultTo(knex.fn.now());
-  });
+  return knex.raw(`
+    
+  `);
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable("user_profile");
+  return knex.raw(`
+ 
+  `);
 };
+// exports.up = function(knex, Promise) {
+//   return knex.schema.createTable("user_profile", table => {
+//     table
+//       .increments("id")
+//       .references("id")
+//       .inTable("users");
+//     table.string("company_name");
+//     table.string("website");
+//     table.string("job_location");
+//     table.string("status").notNullable();
+//     table.string("bio");
+//     table.string("youtube_handle");
+//     table.string("twitter_handle");
+//     table.string("facebook_handle");
+//     table.string("instagram_handle");
+//     table
+//       .timestamp("created_at")
+//       .notNullable()
+//       .defaultTo(knex.fn.now());
+//     table
+//       .timestamp("updated_at")
+//       .notNullable()
+//       .defaultTo(knex.fn.now());
+//   });
+// };
+
+// exports.down = function(knex, Promise) {
+//   return knex.schema.dropTable("user_profile");
+// };
