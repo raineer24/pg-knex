2ef390dbb55df14167853521e43bf23f102f308f
wtf <delaritaraineer81@gmail.com>
2022-02-23 19:59:34 +0800

    prod key

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,1 +1,18 @@
-JWT_SECRET_PROD=secret123
+require("dotenv").config();
+
+module.exports = {
+  env: {
+    port: process.env.PORT || "3000"
+    //hostname: "ner-web.com"
+  },
+  mail: {
+    host: "smtp.gmail.com",
+    port: 587,
+    username: "	beybladebolo24@gmail.com",
+    password: "batman489~"
+  },
+  bycryptSalt: 13,
+  secret_key: "123456",
+  githubClientId: "6c8af49d7044b5cb7842",
+  githubToken:"e047ecc2218acc637590d9eccf0f2c928c784bbb"
+};
