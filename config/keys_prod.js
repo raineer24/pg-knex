5127f928d38f060c0b264b5a9836d41caf4ab801
wtf <delaritaraineer81@gmail.com>
2022-02-23 20:18:44 +0800

    secret wars

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,18 +1,4 @@
-require("dotenv").config();
-
-module.exports = {
-  env: {
-    port: process.env.PORT || "3000"
-    //hostname: "ner-web.com"
-  },
-  mail: {
-    host: "smtp.gmail.com",
-    port: 587,
-    username: "	beybladebolo24@gmail.com",
-    password: "batman489~"
-  },
-  bycryptSalt: 13,
-  secret_key: "123456",
-  githubClientId: "6c8af49d7044b5cb7842",
-  githubToken:"e047ecc2218acc637590d9eccf0f2c928c784bbb"
-};
+if( process.env.NODE_ENV !== 'production' ){
+    require( 'dotenv' ).config()
+}
+const secret = process.env.JWT_SECRET_PROD;
--- config/passport.js
+++ config/passport.js
@@ -2,13 +2,10 @@ const JwtStrategy = require("passport-jwt").Strategy,
   ExtractJwt = require("passport-jwt").ExtractJwt;
 const User = require("../models/users");
 const keys = require("../config/keys");
-const isProduction = process.env.NODE_ENV === 'production';
-const secret = isProduction ? process.env.JWT_SECRET_PROD : process.env.JWT_SECRET_DEV;
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-// opts.secretOrKey = keys.secret_key;
-opts.secretOrKey = secret;
+opts.secretOrKey = keys.secret_key;
 
 module.exports = passport => {
   passport.use(
