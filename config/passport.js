0c5c798b40df28940d81404c18982cbfda9c57eb
wtf <delaritaraineer81@gmail.com>
2022-02-23 19:18:59 +0800

    passport secretKey

--- config/passport.js
+++ config/passport.js
@@ -2,8 +2,8 @@ const JwtStrategy = require("passport-jwt").Strategy,
   ExtractJwt = require("passport-jwt").ExtractJwt;
 const User = require("../models/users");
 const keys = require("../config/keys");
-const secret =
-  config == 'production' ? process.env.SERVER_SECRET_KEY : 'shinedme';
+const isProduction = process.env.NODE_ENV === 'production';
+const secret = isProduction ? process.env.JWT_SECRET_PROD : process.env.JWT_SECRET_DEV;
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
