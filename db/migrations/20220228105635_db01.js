1b8c7294a4640e3254935d25ec6fe68275c08fac
wtf <delaritaraineer81@gmail.com>
2022-03-08 14:27:37 +0800

    heroku migration

--- db/migrations/20220228105635_db01.js
+++ db/migrations/20220228105635_db01.js
@@ -1,11 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.raw(`
   
-  DROP TABLE user_skill_set;
-  DROP TABLE user_experience_detail;
-  DROP TABLE blogs;
-  DROP TABLE user_profile;
-  DROP TABLE post;
+  
   DROP TABLE users;
   
   `);
@@ -13,12 +9,7 @@ exports.up = function(knex, Promise) {
 
 exports.down = function(knex, Promise) {
   return knex.raw(`
-  
-  DROP TABLE user_skill_set;
-  DROP TABLE user_experience_detail;
-   DROP TABLE blogs;
-  DROP TABLE user_profile;
-  DROP TABLE post;
+ 
   DROP TABLE users;
   
   
